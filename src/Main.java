cf204b7a4c56434c22a347c1d6013f34779581dd
Arthur <arthurpoplavec01@gmail.com>
2025-01-26 18:23:33 +0300

    Удаление лишнего с ветки

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        bubbleSortAndBinarySearch.StartTask.start();
-        System.out.println("\n---------------------------------------------------------------------------------------");
-        insertionSort.StartTask.start();
-        System.out.println("\n---------------------------------------------------------------------------------------");
-        quickSort.StartTask.start();
-        System.out.println("\n---------------------------------------------------------------------------------------");
-        stream.StartTask.start();
-        System.out.println("\n---------------------------------------------------------------------------------------");
-        treeTraversal.StartTask.start();
-
+        System.out.println("Запуск проекта!");
     }
 }
--- src/constants/MassConst.java
+++
@@ -1,5 +0,0 @@
-package constants;
-
-public class MassConst {
-    public static final int[] ARRAY = {64, 34, 25, 12, 22, 11, 90, 32, 65, 10, 23, 45, 76, 29, 40, 26};
-}
